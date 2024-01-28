59a5cc3f86e64d7fcbb91ae4489a2062fc477387
Natalia-Havashi <natali.gavashi@gmail.com>
2024-01-28 14:58:48 +0200

    correction

--- models/Contact.js
+++ models/Contact.js
@@ -26,14 +26,12 @@ const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     phone: Joi.string().required(),
-    favorite: Joi.boolean(),
   });
   
   const updateContactSchema = Joi.object({
     name: Joi.string(),
     email:Joi.string(),
     phone:Joi.string(),
-    favorite: Joi.boolean(),
   });
 
   const contactUpdateFavoriteSchema = Joi.object({
